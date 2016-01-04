2fb3219374436f53553530888e019bf635682ab4
tanhuiya <403554355@qq.com>
2016-01-04 21:52:26 +0800

    bug fix 6

---
+++ ThPullRefresh/UIScrollView+ThRefresh.swift
@@ -0,0 +1,95 @@
+//
+//  UIScrollView+ThRefresh.swift
+//  PullRefresh
+//
+//  Created by tanhui on 15/12/29.
+//  Copyright © 2015年 tanhui. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIScrollView{
+    private struct AssociatedKeys {
+        static var ThHeadRefreshName = "ThHeadRefreshName"
+        static var ThFootRefreshName = "ThFootRefreshName"
+
+    }
+    func headBeginRefresh(){
+        self.head!.beginRefresh()
+    }
+    func addHeadRefresh(target : AnyObject?,action : Selector){
+        let head = ThHeadArrowRefreshView()
+        self.head = head
+        self.addSubview(head)
+        head.refreshTarget = target
+        head.refreshAction = action
+    }
+    func addHeadRefresh(target : AnyObject? ,closure : ()->()){
+        let head = ThHeadArrowRefreshView()
+        self.head = head
+        self.addSubview(head)
+        head.refreshTarget = target
+        head.refreshClosure = closure
+    }
+    var head : ThHeadArrowRefreshView? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.ThHeadRefreshName) as? ThHeadArrowRefreshView
+        }
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(
+                    self,
+                    &AssociatedKeys.ThHeadRefreshName,
+                    newValue ,
+                    .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+                )
+            }
+        }
+    }
+    func tableHeadStopRefreshing(){
+        self.head?.stopRefreshing()
+    }
+    //foot
+    func addFootRefresh(target : AnyObject?,action : Selector){
+        let foot = ThFootRefreshView()
+        self.foot = foot
+        self.addSubview(foot)
+        foot.refreshTarget = target
+        foot.refreshAction = action
+    }
+    func addFootRefresh(target : AnyObject? ,closure : ()->()){
+        let foot = ThFootRefreshView()
+        self.foot = foot
+        self.addSubview(foot)
+        foot.refreshTarget = target
+        foot.refreshClosure = closure
+    }
+    var foot : ThFootRefreshView? {
+        get {
+            return objc_getAssociatedObject(self, &AssociatedKeys.ThFootRefreshName) as? ThFootRefreshView
+        }
+        set {
+            if let newValue = newValue {
+                objc_setAssociatedObject(
+                    self,
+                    &AssociatedKeys.ThFootRefreshName,
+                    newValue ,
+                    .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+                )
+            }
+        }
+    }
+    func tableFootStopRefreshing(){
+        self.foot?.footEndRefreshing()
+    }
+    func tableFootShowNomore(){
+        if(self.istableFootRefreshing()){
+            self.tableFootStopRefreshing()
+        }
+        self.foot?.state = .Nomore
+    }
+    func istableFootRefreshing()->Bool{
+        return self.foot?.state == .Refreshing
+    }
+}
